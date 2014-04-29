583098c83cad3d4aca970b761ec32714890043ab
Feodor Fitsner <feodor@fitsner.com>
2014-04-29 12:28:29 -0700

    Initial project check-in

---
+++ MyWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyWebApp.Startup))]
+namespace MyWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
